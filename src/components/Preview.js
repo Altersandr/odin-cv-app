e1b0f56519dfffc479e5f56c364b66aee8738030
Oleks <archer.opendub@gmail.com>
2023-01-28 22:29:59 +0100

    added boilerplate info for visual testing

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -8,22 +8,98 @@ import "./styles/Body.css";
 export class Preview extends Component {
   render() {
     return (
-      <>
+      <div id="preview">
         <div id="left">
           <div id="photo">Photo</div>
           <div id="contact">
             <h1>Contact</h1>
-            <img src="/"></img>
+            {/* image icons and contact info */}
           </div>
-          <div id="skills"></div>
-          <div id="education"></div>
+          <div id="skills">
+            {/* <ul>
+
+            </ul> */}
+          </div>
+          <div id="education">
+            <h1>Education</h1>
+            <div>
+              <h3>Secondary school</h3>
+              <h4>Really Great High School</h4>
+              <h4>2010 - 2014</h4>
+            </div>
+
+            <div>
+              <h3>Bachelor of Technology</h3>
+              <h4>Really Great University</h4>
+              <h4>2010 - 2014</h4>
+            </div>
+          </div>
         </div>
         <div id="right">
-          <div id="name-and-title"></div>
-          <div id="profile"></div>
-          <div id="experience"></div>
+          <div id="name-and-title">
+            <h1>Oleksandr Tsurkan</h1>
+            <h3>Junior Web Developer</h3>
+          </div>
+          <div id="profile">
+            <h1>Profile</h1>
+            <p>
+              {" "}
+              I am a qualified and professional web developer with five years of
+              experience in database administration and website design. Strong
+              creative and analytical skills. Team player with an eye for
+              detail.{" "}
+            </p>
+          </div>
+          <div id="experience">
+            <div>
+              <h3>Applications developer</h3>
+              <h4>Really Great Company</h4>
+              <h4>2016 - Present</h4>
+              <ul>
+                <li>Database administration and website design</li>
+                <li>
+                  Built the logic for a streamlined ad-serving platform that
+                  scaled
+                </li>
+                <li>
+                  Educational institutions and online classroom management
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Web content manager</h3>
+              <h4>Really Great Company</h4>
+              <h4>2014 - 2016</h4>
+              <ul>
+                <li>Database administration and website design</li>
+                <li>
+                  Built the logic for a streamlined ad-serving platform that
+                  scaled
+                </li>
+                <li>
+                  Educational institutions and online classroom management
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>Analysis content</h3>
+              <h4>Really Great Company</h4>
+              <h4>2010 - 2014</h4>
+              <ul>
+                <li>Database administration and website design</li>
+                <li>
+                  Built the logic for a streamlined ad-serving platform that
+                  scaled
+                </li>
+                <li>
+                  Educational institutions and online classroom management
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </>
+      </div>
     );
   }
 }
